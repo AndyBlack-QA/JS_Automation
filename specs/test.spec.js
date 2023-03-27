771557b5c41da53c5c18661b54c71999d1560ac3
Andrei_hudkou2 <Andrei_hudkou2@epam.com>
2023-03-27 19:14:45 +0300

    Done

--- specs/test.spec.js
+++ specs/test.spec.js
@@ -6,8 +6,8 @@ const {
 const testData = require("../config/data.json");
 const { expect } = require("chai");
 
-describe("API test suite", ()=>{
 //Read (using get() method): list all resources / get a resource / filter resources;
+describe("API test suite", ()=>{
     it ("GET post /1", async () => {
         const response = await sendRequest("posts/1")
         expect(response.data.userId).to.equal(1);
@@ -15,7 +15,7 @@ describe("API test suite", ()=>{
     });
 
     it("Post posts",async()=>{
-        const response = await sendRequest("posts",testData.first ,"post");
+        const response = await sendRequest("posts",testData,"post");
         expect(response.status).to.equal(201);
     });
 
@@ -26,12 +26,12 @@ describe("API test suite", ()=>{
             "body": "bar",
             "userId": 455
         }
-
         const response = await sendRequest("posts",testingData,"post")
         expect(response.data.title).to.equal("Andy")
         expect(response.data.userId).to.equal(455)
         expect(response.data.userId).not.to.equal(500)
     })
+    
 //Update a resource (using put() method);
     it("updateResourse",async()=>{
         testikData = {
@@ -41,12 +41,18 @@ describe("API test suite", ()=>{
         }
         const responsik = await sendRequest("posts/1",testikData,"put")
         expect(responsik.data.title).to.equal("updated")
-        
     })
+
 //Delete a resource (using delete() method);
     it("deleteResourse",async()=>{
         const responsikk = await sendRequest("posts/1",null,"delete")
         expect(responsikk.status).to.equal(200)
         expect(responsikk.data).to.eql({ })
     })
+
+//negative scenario
+    it ("GET post /700", async () => {
+        const response = await sendRequest("posts/700")
+        expect(response.status).to.equal(404)
+    });
 });
